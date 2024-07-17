429fd32ded92780a3be32e255f16334d22b46742
Brady Friese <42753309+bmf4295@users.noreply.github.com>
2024-07-17 10:53:10 +0900

    Started work on navBar

--- bf-resume-site/src/App.js
+++ bf-resume-site/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import React from 'react';
+import NavBar from './components/navBar'
+import Home from './components/homePage'
+import Skils from './components/skillsPage'
+import About from './components/aboutPage'
+import Experience from './components/experiencePage'
+import Contact from './components/contactPage'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/skills' element={<Skils/>}/>
+          <Route path='/experience' element={<Experience/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
