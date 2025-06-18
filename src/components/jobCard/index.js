bc17e3b5be4a35a83afff9703d59e39e8ff73699
Brady Friese <bfriese1207@gmail.com>
2025-06-18 17:23:09 -0500

    miscellaneous cleanup and changed background image

--- src/components/jobCard/index.js
+++ src/components/jobCard/index.js
@@ -34,7 +34,7 @@ const JobCard = ({ title, period, company, responsibilities }) => {
             <div className="absolute left-8 transform -translate-x-1/2 w-8 h-8 bg-white dark:bg-gray-800 rounded-full border-2 border-blue-500 flex items-center justify-center">
                 <i className="fas fa-briefcase text-blue-500"></i>
             </div>
-            <div className="ml-20 p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg">
+            <div className="ml-20 p-6 bg-white dark:bg-gray-900 rounded-lg shadow-lg">
                 <div className="flex justify-between items-center mb-3">
                     <h3 className="text-xl font-bold dark:text-white">{title}</h3>
                     <span className="text-gray-600 dark:text-gray-400">{period}</span>
--- src/components/skillsPage/index.js
+++ src/components/skillsPage/index.js
@@ -4,13 +4,13 @@ const Skills = () => {
     const skillCategories = {
         "Programming Languages": [
             { name: "HTML/CSS", level: 95 },
-            { name: "JavaScript", level: 90 },
-            { name: "TypeScript", level: 90 },
+            { name: "JavaScript/TypeScript", level: 90 },
             { name: "Java", level:85 },
             { name: "SQL", level:85 },
             { name: "C++", level: 85 },
             { name: "Python", level: 80 },
             { name: "C#", level: 75 },
+             { name: "PL/SQL", level: 70 },
         ],
         "Frameworks & Libraries": [
             { name: "React", level: 95 },
@@ -23,7 +23,9 @@ const Skills = () => {
         "Tools & Platforms": [
             { name: "MongoDB", level: 95 },
             { name: "PostgresSQL", level: 90 },
+            { name: "Github Copilot", level: 90 },
             { name: "Oracle Database", level: 85 },
+            { name: "Spring Boot", level: 80 },
             { name: "Google Firebase", level: 70 },
         ],
     };
@@ -31,7 +33,7 @@ const Skills = () => {
     return (
         <div className="flex flex-col bg-inherit min-h-screen pt-16" id="Skills">
             <div className="max-w-4xl mx-auto w-full flex flex-col gap-6">
-                <h2 className="text-4xl font-bold mb-6 dark:text-white text-center">Skills</h2>
+                <h2 className="text-4xl font-bold mb-6 dark:text-white text-center">Skill Competencies</h2>
                 
                 {Object.entries(skillCategories).map(([category, skills]) => (
                     <div key={category} className="p-4 bg-white dark:bg-gray-800 rounded-lg shadow-lg">
